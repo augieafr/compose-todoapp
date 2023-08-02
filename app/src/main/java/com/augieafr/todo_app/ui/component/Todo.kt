05b626cd71e85150831531cb5352c808ceff8722
Augie Afriyansyah <augieafriyansyah@gmail.com>
2023-08-02 16:52:14 +0700

    refactor(todo): - change all TodoUiModel property to val to make it stable - create todo list screen - add todo event add

--- app/src/main/java/com/augieafr/todo_app/ui/component/Todo.kt
+++ app/src/main/java/com/augieafr/todo_app/ui/component/Todo.kt
@@ -1,6 +1,5 @@
 package com.augieafr.todo_app.ui.component
 
-import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,15 +15,19 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,30 +35,46 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.augieafr.todo_app.ui.model.TodoUiModel
-import com.augieafr.todo_app.utils.dueDateToDeadline
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ToDoList(modifier: Modifier, todoList: List<TodoUiModel>, onTodoEvent: (ToDoEvent, Int) -> Unit) {
-    LazyColumn(
-        modifier = modifier,
-    ) {
-        itemsIndexed(todoList) { index, todo ->
-            ToDo(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-                    .height(IntrinsicSize.Max),
-                todoModel = todo,
-                onTodoEvent = { event ->
-                    onTodoEvent.invoke(event, index)
-                }
-            )
+fun ToDoListScreen(
+    modifier: Modifier,
+    todoList: List<TodoUiModel>,
+    onTodoEvent: (Int, ToDoEvent) -> Unit
+) {
+    // A surface container using the 'background' color from the theme
+    Scaffold(modifier = modifier, topBar = {
+        TopAppBar(title = { Text(text = "KMM ToDo") })
+    }, floatingActionButton = {
+        FloatingActionButton(onClick = {
+            onTodoEvent.invoke(0, ToDoEvent.Add)
+        }) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+        }
+    }) { paddingValues ->
+
+        LazyColumn(
+            modifier = Modifier.padding(paddingValues),
+        ) {
+            itemsIndexed(todoList) { index, todo ->
+                ToDo(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .height(IntrinsicSize.Max),
+                    todoModel = todo,
+                    onTodoEvent = { event ->
+                        onTodoEvent.invoke(index, event)
+                    }
+                )
+            }
         }
     }
 }
+
 @Composable
 fun ToDo(
     todoModel: TodoUiModel,
@@ -64,7 +83,7 @@ fun ToDo(
 ) {
     val cardColor: Color
     val textColor: Color
-    var textDecoration: TextDecoration
+    val textDecoration: TextDecoration
 
     if (todoModel.isDone) {
         with(MaterialTheme.colorScheme) {
@@ -81,7 +100,7 @@ fun ToDo(
     }
 
     Card(
-        modifier = modifier.clickable { onTodoEvent.invoke(ToDoEvent.Detail(todoModel)) },
+        modifier = modifier.clickable { onTodoEvent.invoke(ToDoEvent.Detail) },
         colors = CardDefaults.cardColors(
             containerColor = cardColor
         )
@@ -96,7 +115,7 @@ fun ToDo(
             Checkbox(
                 modifier = Modifier.size(24.dp),
                 checked = todoModel.isDone,
-                onCheckedChange = { onTodoEvent.invoke(ToDoEvent.Done(todoModel)) })
+                onCheckedChange = { onTodoEvent.invoke(ToDoEvent.Done(it)) })
             Spacer(modifier = Modifier.size(8.dp))
             Divider(
                 Modifier
@@ -120,7 +139,7 @@ fun ToDo(
                 modifier = Modifier
                     .align(Alignment.Top)
                     .clickable {
-                        onTodoEvent.invoke(ToDoEvent.Delete(todoModel))
+                        onTodoEvent.invoke(ToDoEvent.Delete)
                     },
                 imageVector = Icons.Default.Delete,
                 contentDescription = "Delete button",
@@ -144,26 +163,9 @@ fun ToDo(
     }
 }
 
-sealed class ToDoEvent(val todoModel: TodoUiModel) {
-    class Delete(todo: TodoUiModel) : ToDoEvent(todo)
-    class Detail(todo: TodoUiModel) : ToDoEvent(todo)
-    class Done(todo: TodoUiModel) : ToDoEvent(todo)
-}
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-fun ToDoPreview() {
-
-    Surface {
-        ToDo(modifier = Modifier
-            .fillMaxWidth()
-            .height(IntrinsicSize.Max), todoModel = TodoUiModel(
-            1,
-            title = "Preview",
-            description = "This is preview",
-            dueDate = "20230705",
-            deadLine = dueDateToDeadline("20230705"),
-            isDone = true
-        ), onTodoEvent = {})
-    }
+sealed class ToDoEvent {
+    object Delete : ToDoEvent()
+    object Detail : ToDoEvent()
+    class Done(val isDone: Boolean) : ToDoEvent()
+    object Add : ToDoEvent()
 }
