caca3fc5ae3d0bb6f78f86435bc2353f84f6df37
Augie Afriyansyah <augieafriyansyah@gmail.com>
2023-08-07 21:11:25 +0700

    refactor(data): change all dao function to suspend to safe call room query with coroutine

--- app/src/main/java/com/augieafr/todo_app/data/repository/TodoRepository.kt
+++ app/src/main/java/com/augieafr/todo_app/data/repository/TodoRepository.kt
@@ -4,23 +4,24 @@ import com.augieafr.todo_app.data.dao.TodoDao
 import com.augieafr.todo_app.data.mapper.toTodoEntity
 import com.augieafr.todo_app.data.mapper.toTodoUiModel
 import com.augieafr.todo_app.ui.model.TodoUiModel
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class TodoRepository @Inject constructor(private val todoDao: TodoDao) {
-    suspend fun getTodos() = flow<List<TodoUiModel>> {
-        todoDao.getTodos().collectLatest {
-            val listTodoUiModel = mutableListOf<TodoUiModel>()
-            it.forEach { todoEntity ->
-                listTodoUiModel.add(todoEntity.toTodoUiModel())
+    suspend fun getTodos() =
+        flow<List<TodoUiModel>> {
+            val todoList = mutableListOf<TodoUiModel>()
+            todoDao.getTodos().forEach {
+                val todo = it.toTodoUiModel()
+                todoList.add(todo)
             }
-            emit(listTodoUiModel)
+            emit(todoList)
         }
-    }
 
-    fun addEditTodo(todoUiModel: TodoUiModel) = todoDao.addUpdateTodo(todoUiModel.toTodoEntity())
+    suspend fun addEditTodo(todoUiModel: TodoUiModel) =
+        todoDao.addUpdateTodo(todoUiModel.toTodoEntity())
 
 
-    fun deleteTodo(todoUiModel: TodoUiModel) = todoDao.deleteTodo(todoUiModel.toTodoEntity())
+    suspend fun deleteTodo(todoUiModel: TodoUiModel) =
+        todoDao.deleteTodo(todoUiModel.toTodoEntity())
 }
