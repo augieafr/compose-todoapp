5bcc7a906f9014dcd8e802473f6673d4d84726da
Augie Afriyansyah <augieafriyansyah@gmail.com>
2023-08-07 20:04:50 +0700

    feat(di): create database and repository module

---
+++ app/src/main/java/com/augieafr/todo_app/data/di/DataModule.kt
@@ -0,0 +1,39 @@
+package com.augieafr.todo_app.data.di
+
+import android.content.Context
+import androidx.room.Room
+import com.augieafr.todo_app.data.dao.TodoDao
+import com.augieafr.todo_app.data.db.TodoDatabase
+import com.augieafr.todo_app.data.repository.TodoRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(appContext, TodoDatabase::class.java, "todo_db").build()
+
+    @Provides
+    @Singleton
+    fun provideTodoDao(todoDatabase: TodoDatabase): TodoDao {
+        return todoDatabase.todoDao()
+    }
+}
+
+@InstallIn(ViewModelComponent::class)
+@Module
+class RepositoryModule {
+    @Provides
+    @Singleton
+    fun provideTodoRepository(todoDao: TodoDao): TodoRepository {
+        return TodoRepository(todoDao)
+    }
+}
